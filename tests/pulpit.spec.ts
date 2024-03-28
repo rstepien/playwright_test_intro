0669aa37baf6112e76ebc0b5514ccd7062f623fa
rstepien <67024204+rstepien@users.noreply.github.com>
2024-03-28 11:35:09 +0100

    add balance test

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -4,7 +4,7 @@ test.describe('Pulpit Demobank tests ', () => {
   test.beforeEach(async ({ page }) => {
     const userId = 'testerLO';
     const userPassword = '87iokjhn';
-    
+
     await page.goto('/');
     await page.getByTestId('login-input').fill(userId);
     await page.getByTestId('password-input').fill(userPassword);
@@ -67,6 +67,26 @@ test.describe('Pulpit Demobank tests ', () => {
     await expect(page.locator('#show_messages')).toHaveText(
       `Doładowanie wykonane! ${transferAmount},00PLN na numer ${receiverNumber}`,
     );
+  });
+  test('correct balance after successful mobile topup with fill amount', async ({
+    page,
+  }) => {
+    // Arrange
+    const receiverNumber = '503 xxx xxx';
+    const transferAmount = '50';
+    const expectedMessage = `Doładowanie wykonane! ${transferAmount},00PLN na numer ${receiverNumber}`;
+    const initialBalance = await page.locator('#money_value').innerText();
+    const expectedBalance = Number(initialBalance) - Number(transferAmount);
+
+    // Act
+    await page.locator('#widget_1_topup_receiver').selectOption(receiverNumber);
+    await page.locator('#widget_1_topup_amount').fill(transferAmount);
+    await page.locator('#uniform-widget_1_topup_agreement span').click();
+    await page.getByRole('button', { name: 'doładuj telefon' }).click();
+    await page.getByTestId('close-button').click();
+
+    // Assert
+    await expect(page.locator('#money_value')).toHaveText(`${expectedBalance}`);
   });
   test.skip('successful mobile topup with select amount', async ({ page }) => {
     await page.locator('#widget_1_topup_receiver').selectOption('504 xxx xxx');
